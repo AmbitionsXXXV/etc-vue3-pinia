84ce3e989b85c38f4997703d2b48a946e181b97f
Etcetera <yilonglei790@gmail.com>
2022-09-21 12:43:34 +0800

    暂时完成

--- components.d.ts
+++ components.d.ts
@@ -17,12 +17,18 @@ declare module '@vue/runtime-core' {
     TabBar: typeof import('./src/components/tab-bar/tab-bar.vue')['default']
     TabControl: typeof import('./src/components/tab-control/tab-control.vue')['default']
     VanCalendar: typeof import('vant/es')['Calendar']
+    VanCell: typeof import('vant/es')['Cell']
     VanIcon: typeof import('vant/es')['Icon']
+    VanIndexAnchor: typeof import('vant/es')['IndexAnchor']
+    VanIndexBar: typeof import('vant/es')['IndexBar']
     VanNavBar: typeof import('vant/es')['NavBar']
     VanRate: typeof import('vant/es')['Rate']
+    VanSearch: typeof import('vant/es')['Search']
     VanSwipe: typeof import('vant/es')['Swipe']
     VanSwipeItem: typeof import('vant/es')['SwipeItem']
+    VanTab: typeof import('vant/es')['Tab']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
+    VanTabs: typeof import('vant/es')['Tabs']
   }
 }
